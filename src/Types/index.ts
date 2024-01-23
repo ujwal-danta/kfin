2f374ce38eefdd139a67c09d1c87504681be44a9
Satyajeet Tripathy <8001908@kfintech.com>
2024-01-23 12:12:58 +0530

    added formik with ts

--- src/Types/index.ts
+++ src/Types/index.ts
@@ -1,17 +1,28 @@
-
-
-export type Todo ={
-    userId:number,
-    id:number,
-    title:string,
-    completed:boolean
-}
-export type FetchComponentType ={
-    data?:Todo
-}
+import { FieldInputProps } from "formik";
+export type Todo = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
+export type FetchComponentType = {
+  data?: Todo;
+};
 
 export type ButtonProps = {
-    name:string,
-    ButtonClick: ()=>void
+  name: string;
+  ButtonClick: () => void;
+};
+export interface FeildLabelsProps {
+  [key: string]: string; // You have to define what kind of index type the object has
+  firstName: string;
+  email: string;
+  lastName: string;
 }
-
+export interface TextInputProps extends FieldInputProps<string> {
+  label: string;
+  onValueChange: (value: string) => void;
+  type: string;
+  id?: string; //added optional type for id and placeholder
+  placeholder?: string;
+}
--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,27 +1,26 @@
-import { FieldInputProps, Form, Formik } from "formik";
+import { Form, Formik, FormikHelpers } from "formik";
 import React, { useState } from "react";
-import * as Yup from 'yup';
+import * as Yup from "yup";
 import TextInput from "../TextInput/TextInput";
-
-interface FeildLabelsProps {
-  firstName:string,
-  email:string,
-  lastName:string,
-}
+import { FeildLabelsProps } from "../../Types";
 
 const LoginForm = React.memo(() => {
-  const fieldLabels:FeildLabelsProps = {
-    firstName: 'First Name',
-    email: 'Email',
-    lastName: 'Last Name',
+  const fieldLabels: FeildLabelsProps = {
+    firstName: "First Name",
+    email: "Email",
+    lastName: "Last Name",
   };
-  const [formValues, setFormValues] = useState({
-    firstName: '',
-    email: '',
-    lastName: '',
+  const [formValues, setFormValues] = useState<FeildLabelsProps>({
+    firstName: "",
+    email: "",
+    lastName: "",
   });
-
-  const updateFormValues = (fieldName:string, value:string) => {
+  const initialValues: FeildLabelsProps = {
+    firstName: "",
+    email: "",
+    lastName: "",
+  };
+  const updateFormValues = (fieldName: string, value: string) => {
     setFormValues({
       ...formValues,
       [fieldName]: value,
@@ -29,50 +28,52 @@ const LoginForm = React.memo(() => {
   };
   return (
     <>
-    <Formik
-      initialValues={{
-        firstName: '',
-        email: '',
-        lastName: '',
-      }}
-      validationSchema={Yup.object({
-        firstName: Yup.string()
-          .max(15, 'Must be 15 characters or less')
-          .required('Required'),
-        email: Yup.string()
-          .email('Invalid email address')
-          .required('Required'),
-        lastName: Yup.string()
-          .max(5, 'Must be 5 characters or less')
-          .min(3, "Must be 3 characters or more")
-          .required('Required'),
-      })}
-      onSubmit={(values, { setSubmitting }) => {
-        setTimeout(() => {
-          alert(JSON.stringify(values, null, 2));
-          setSubmitting(false);
-        }, 400);
-      }}
-    >
-      {({ values, handleChange }) => (
-        <Form>
-          {Object.keys(values).map((fieldName:string) => (
-            <TextInput
-            key={fieldName}
-            label={fieldLabels[fieldName]}
-            name={fieldName}
-            type="text"
-            placeholder={`Enter your ${fieldLabels[fieldName]}`}
-            onChange={handleChange}
-            value={values[fieldName]}
-              onValueChange={(value) => updateFormValues(fieldName, value)}
-            />
-          ))}
-          <button type="submit">Submit</button>
-        </Form>
-      )}
-    </Formik>
-  </>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={Yup.object({
+          firstName: Yup.string()
+            .max(15, "Must be 15 characters or less")
+            .required("Required"),
+          email: Yup.string()
+            .email("Invalid email address")
+            .required("Required"),
+          lastName: Yup.string()
+            .max(5, "Must be 5 characters or less")
+            .min(3, "Must be 3 characters or more")
+            .required("Required"),
+        })}
+        onSubmit={(
+          values,
+          { setSubmitting }: FormikHelpers<FeildLabelsProps> // added formik helpers for the setSubmitting func</FeildLabelsProps>
+        ) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 400);
+        }}
+      >
+        {({ values, handleChange }) => (
+          <Form>
+            {Object.keys(values).map((fieldName: string) => (
+              <TextInput
+                key={fieldName}
+                label={fieldLabels[fieldName]}
+                name={fieldName}
+                type="text"
+                placeholder={`Enter your ${fieldLabels[fieldName]}`}
+                onChange={handleChange}
+                value={values[fieldName]}
+                onValueChange={(value: string) =>
+                  updateFormValues(fieldName, value)
+                }
+                onBlur={() => {}} // added onBLur prop (Review)
+              />
+            ))}
+            <button type="submit">Submit</button>
+          </Form>
+        )}
+      </Formik>
+    </>
   );
 });
 
